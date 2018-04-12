4d887e740a265d45ba545f6c96e52c5760b62cf4
barestu <fheartnet@gmail.com>
2018-04-12 21:12:11 +0700

    Progress front end

--- client/src/js/app-main.js
+++ client/src/js/app-main.js
@@ -1,6 +1,36 @@
-new Vue ({
+new Vue({
   el: '#app',
   data: {
-    message: 'Welcome to Cardify'
+    name: '',
+    company: '',
+    companyName: '',
+    companyAddress: '',
+    image: ''
+  },
+  methods: {
+    formData(event){
+      this.name = event.target.elements.name.value
+      this.email = event.target.elements.email.value
+      this.companyName = event.target.elements.companyName.value
+      this.companyAddress = event.target.elements.companyAddress.value
+      event.target.reset()
+    },
+
+    onFileChange(e) {
+      var files = e.target.files || e.dataTransfer.files;
+      console.log(files)
+      // if (!files.length)
+      // return
+      this.createImage(files[0])
+    },
+    
+    createImage(file) {
+      var image = new Image()
+      var reader = new FileReader()
+      reader.onload = (e) => {
+        this.image = e.target.result;
+      };
+      reader.readAsDataURL(file)
+    }
   }
 })
