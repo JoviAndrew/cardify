a65c50225f394a24fca0715b5a65f23b301ffae1
faizal <faizalramanda@gmail.com>
2018-04-12 22:28:00 +0700

    login fb lagi

--- server/routers/index.js
+++ server/routers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const index = require('../controllers/index_controller');
 
 //Register new User
-router.post('/register', index.registerUser);
+// router.post('/register', index.registerUser);
 
 //Login User
 router.post('/login', index.loginUser);
