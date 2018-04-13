bbaec520ba58dfa30420c465cc40e0c5dd3311e6
JoviAndrew <jovian_andrew@hotmail.com>
2018-04-13 11:25:36 +0700

    resolve conflict

--- server/controllers/index_controller.js
+++ server/controllers/index_controller.js
@@ -15,12 +15,8 @@ module.exports = {
             const newUser = new User(req.body)
             newUser.save()
             .then(data => {
-<<<<<<< HEAD
               let token = jwt.sign({ id: data._id, name: data.name }, process.env.SECRET);
 
-=======
-              let token = jwt.sign({ id: data._id, name: data.name }, 'shhhhh');
->>>>>>> 1f043113442ed46033413ed0e4bac81bea0fabb5
               res.status(200).json({
                 token: token
               })
@@ -28,74 +24,5 @@ module.exports = {
             })
           }
         })
-<<<<<<< HEAD
     }
-=======
-
-        //     users.findOne({
-        //         username: req.body.email
-        //     })
-        //     .then(function(userData){
-        //         if(userData != null){
-        //             res.status(400).json({
-        //                 message: "username has been taken!",
-        //             })
-        //         }else{
-        //             let salt = bcrypt.genSaltSync(saltRounds)
-        //             let hash = bcrypt.hashSync(password, salt);
-        //                 users
-        //                 .create({
-        //                     username: req.body.username,
-        //                     password: hash,
-        //                 })
-        //                 .then(function(result){
-        //                     res.status(200).json({
-        //                         message: "success register a new user",
-        //                         result: result
-        //                     })
-        //                 })
-        //                 .catch(function(err){
-        //                     res.status(500).json({
-        //                         message: err
-        //                     })
-        //                 })
-        //         }
-        //     })
-        //     .catch(function(err){
-        //         res.status(500).json({
-        //             message: err
-        //         })
-        //     })
-        //
-        // }
-  }
-    // ,
-    // loginUser: function(req, res){
-    //   console.log(req.body);
-        // users.findOne({
-        //     username: req.body.username
-        // })
-        // .then(function(userData){
-        //     if(!userData){
-        //         res.status(400).json({
-        //             message: 'incorrect username or password'
-        //         })
-        //     }else{
-        //         bcrypt.compare(req.body.password, userData.password, function(err, result){
-        //             if(!result){
-        //                 res.json({
-        //                     message: 'incorrect username or password'
-        //                 })
-        //             }else{
-        //                 let token = jwt.sign({id: userData._id}, process.env.SECRET)
-        //                 res.json({
-        //                     message: 'Success login',
-        //                     token: token
-        //                 })
-        //             }
-        //         })
-        //     }
-        // })
-    // }
->>>>>>> 1f043113442ed46033413ed0e4bac81bea0fabb5
 }
